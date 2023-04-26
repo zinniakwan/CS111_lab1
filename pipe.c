9ba7cfd4ed4025381b136471dd157104e01a8820
Zinnia Kwan <zinniakwan@ucla.edu>
2023-04-26 11:18:09 -0700

    one argument done

--- pipe.c
+++ pipe.c
@@ -1,4 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
 int main(int argc, char *argv[])
 {
+	execlp(argv[1], argv[1], NULL);
 	return 0;
 }
